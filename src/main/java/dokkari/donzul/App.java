149cd1c345c51c4eabe5889562e37708b497c8ff
kjuhe <kjuhe@DESKTOP-SSEROED>
2018-08-20 22:17:31 +0900

    초기화

---
+++ src/main/java/dokkari/donzul/App.java
@@ -0,0 +1,13 @@
+package dokkari.donzul;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
